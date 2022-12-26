eb5b6f555dfbcd04e34629c481082b6c55912fe4
hnam <hnam4527@gmail.com>
2022-12-26 14:31:08 +0700

    loadingskeleton

--- src/components/loading/loadingSkeleton.jsx
+++ src/components/loading/loadingSkeleton.jsx
@@ -3,7 +3,7 @@ import Loading from "./loading";
 
 const loadingSkeleton = () => {
   return (
-    <div className="w-full pro relative overflow-hidden">
+    <div className="w-full min-w-[300px] pro relative overflow-hidden">
       <Loading width="100%" height="300px" radius="8px"></Loading>
       <div className="pro-bottom flex flex-col justify-center items-center h-10 gap-y-1">
         <div className="max-w-[80%] truncate">
